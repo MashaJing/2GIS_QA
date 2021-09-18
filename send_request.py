bd056a1697b2e9b63dde2532332fa94efcbfe95d
Мария Дзех <mdzekh23032000a@gmail.com>
2021-09-18 19:47:45 +0700

    added first test

--- send_request.py
+++ send_request.py
@@ -4,14 +4,16 @@ from requests import cookies
 def get_token(s):
     request = s.post('https://regions-test.2gis.com/v1/auth/tokens')
     dict_cookie = requests.utils.dict_from_cookiejar(request.cookies)
-    return dict_cookie #токен в виде словаря {'token': 'значение_токена', 'date': 'время создания'}
+    
+    return dict_cookie #токен в виде словаря {'token': 'значение_токена'}
 
 
 def create_place(token, data): 
-    cookies = { 'token' : token }
-    r = requests.post('https://regions-test.2gis.com/v1/favorites', cookies = cookies, data=data)
+    #cookies = { 'token' : token }
+    r = requests.post('https://regions-test.2gis.com/v1/favorites', cookies = token, data=data)
     return r.json()
 
+'''
 s = requests.Session()
 data = {
     'title' : 'Камень',     # title - Название места. Может содержать латинские и кириллические символы, цифры и знаки
@@ -20,5 +22,7 @@ data = {
     'lon': '82.925642',    #lon - Долгота (longitude) места
     'color': 'BLUE'        #color - Цвет иконки места. Может принимать значения: BLUE, GREEN, RED, YELLOW
     }
-dict_token = get_token(s)
-print(create_place(dict_token['token'], data))
+token = get_token(s)
+dict_res = create_place(token, data)
+print(dict_res.keys())
+'''
--- test_send_request.py
+++ test_send_request.py
@@ -1,9 +1,6 @@
-from send_request import create_place, get_token
 import unittest
+from send_request import create_place, get_token
 import requests
-
-#def check_token():
-
 
 class TestCreatePlace(unittest.TestCase):
     def test_request(self):
@@ -11,8 +8,12 @@ class TestCreatePlace(unittest.TestCase):
         token = get_token(s)
         data = {
         'title' : 'Камень',
-        'lat': '55.036500', 
-        'lon': '82.925642',
-        'color': 'BLUE' 
+        'lat': '-55.036500', 
+        'lon': '-82.925642',
+        'color': 'blue' 
         }
-        self.assertDictEqual(create_place(token, data), ...)
+        dict_res = create_place(token, data)
+        print(dict_res)
+        self.assertEqual(dict_res['lon'], -82.925642)
+
+#def check_token():
