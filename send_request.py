f2cab6402eb9a608127b882d2e263414fd4b8a23
Мария Дзех <mdzekh23032000a@gmail.com>
2021-10-02 17:13:47 +0700

    Made req_sender class

--- send_request.py
+++ send_request.py
@@ -1,39 +1,52 @@
-import datetime
+#import datetime
 #import traceback
 import time
 import requests
 from requests import cookies
 
-def get_token(s):
-    request = s.post('https://regions-test.2gis.com/v1/auth/tokens')
-    dict_cookie = requests.utils.dict_from_cookiejar(request.cookies)
-    start_time = time.time() #зафиксируем время получения токена
-    dict_cookie.update({'time' : start_time})
-    return dict_cookie #токен в виде словаря {'token': 'значение_токена', 'time' : время получения токена}
+class req_sneder():
+    #общая сессия для всех запросов
+    s = requests.Session()
+    #токен
+    token = None
+    #время получения токена
+    token_time = None
 
-def refresh_token(func):
-    def wrapper(s, token, data):
-        if time.time() - token['time'] < 1.6: #если от получения токена прошло менее 2 секунд с учетом погрешности
-            #выполняем с тем же токеном
-            return func(token, data)
-        else:
-            # иначе обращаемся к функции и получаем новый токен
-            new_token = get_token(s)
-            # обновив токен, пытаемся повторить действие
-            return func(new_token, data)
-    return wrapper
+    def __init__(self):
+        self.get_token()
+        self.s = requests.Session()
 
-@refresh_token
-def create_place(dict_token, data): 
-    cookies = {'token' : dict_token['token'] }
-    r = requests.post('https://regions-test.2gis.com/v1/favorites', cookies = cookies, data=data)
-    
-    if r.status_code != 200:
-        raise ConnectionError
-    return r.json()
+    #посылает пустой post-запрос для получения токена
+    def get_token(self):
+        request = self.s.post('https://regions-test.2gis.com/v1/auth/tokens')
+        self.token = requests.utils.dict_from_cookiejar(request.cookies)['token']
+        self.token_time = time.time() #зафиксируем время получения токена
+
+    #декоратор для обновления токена по необходимости
+    def refresh_token(func):
+        def wrapper(self, data):
+            if time.time() - self.token_time < 1.6: #если от получения токена прошло менее 2 секунд с учетом погрешности
+                #выполняем с тем же токеном
+                return func(self, data)
+            else:
+                # иначе обращаемся к функции и получаем новый токен
+                self.get_token()
+                # обновив токен, пытаемся повторить действие
+                return func(self, data)
+        return wrapper
+
+    #метод создания избранного места
+    @refresh_token
+    def create_place(self, data): 
+        cookies = {'token' : self.token }
+        r = requests.post('https://regions-test.2gis.com/v1/favorites', cookies = cookies, data=data)
+        
+        if r.status_code != 200:
+            raise ConnectionError
+        return r.json()
 
 '''
-s = requests.Session()
+req = req_sneder()
 data = {
     'title' : 'Камень',     # title - Название места. Может содержать латинские и кириллические символы, цифры и знаки
                             #препинания. Минимальная длина — 1 символ. Максимальная длина — 999 символов
@@ -41,10 +54,12 @@ data = {
     'lon': '82.925642',    #lon - Долгота (longitude) места
     'color': 'BLUE'        #color - Цвет иконки места. Может принимать значения: BLUE, GREEN, RED, YELLOW
     }
-token = get_token(s)
-dict_res = create_place(s, token, data)
+
+dict_res = req.create_place(data)
 print(dict_res)
 time.sleep(2)
-dict_res = create_place(s, token, data)
+dict_res = req.create_place(data)
 print(dict_res)
 '''
+
+
--- test_send_request.py
+++ test_send_request.py
@@ -1,13 +1,14 @@
 from math import inf
 import unittest
-from send_request import create_place, get_token
-import requests
+from send_request import req_sneder
+#import requests
 import re
 
 class TestCreatePlace(unittest.TestCase):
-    
+    #Данные для запросов
     data = {}
-    s = requests.Session()
+    #Объект для отправки запросов и автоматического получения/обновления токенов
+    sender = req_sneder()
 
     def data_init(self):
         self.data = {
@@ -22,9 +23,8 @@ class TestCreatePlace(unittest.TestCase):
 
         try:
             
-            token = get_token(self.s) 
             self.data_init() 
-            create_place(self.s, token, self.data) 
+            self.sender.create_place(self.data) 
         
             
             self.data = {
@@ -32,26 +32,26 @@ class TestCreatePlace(unittest.TestCase):
             'lat': 75, 
             'lon': 44 }
             
-            create_place(self.s, token, self.data)
+            self.sender.create_place(self.data)
 
-            # self.data = {
-            # 'title' : 'Камень',
-            # 'lat': '0.0', 
-            # 'lon': '0.0' }
+            self.data = {
+            'title' : 'Камень',
+            'lat': '0.0', 
+            'lon': '0.0' }
             
-            # create_place(self.s, token, self.data) #500
+            self.sender.create_place(self.data) #ошибка 500
 
             self.data = {
             'title' : 'Камень',
             'lat': 89, 
             'lon': 179 }
-            create_place(self.s, token, self.data)
+            self.sender.create_place(self.data)
             
             self.data = {
             'title' : 'Камень',
             'lat': 9, 
             'lon': 10 }
-            create_place(self.s, token, self.data)
+            self.sender.create_place(self.data)
 
         except ConnectionError:
             self.fail("test_request() raised ConnectionError unexpectedly!")
@@ -59,34 +59,33 @@ class TestCreatePlace(unittest.TestCase):
 
     def test_id(self):
         #каждый тест строится в 2 этапа:
-        token = get_token(self.s)
         self.data_init() #1) инициализация данных для запроса
         res = []
-        #2) Изучение резултатов запроса: например, проверим монотонное возрастание индекса
-        res.append(create_place(self.s, token, self.data)) 
+        #2) Изучение резултатов запроса: например, проверим монотонное возрастание индекса и его тип
+        res.append(self.sender.create_place(self.data)) 
         for i in range(1):
-            res.append(create_place(self.s, token, self.data))
+            res.append(self.sender.create_place(self.data))
             self.assertGreater(res[i+1]['id'], res[i]['id'])
+            self.assertEqual(type(res[i]['id']), int)
 
 
     def test_title(self):
-        token = get_token(self.s)
         #занулим заголовок
-        #{ 'title' : None, 'lat': '55.036500', 'lon': '82.925642' }
         self.data_init()
         self.data['title'] = None
         with self.assertRaises(ConnectionError):
-            dict_res = create_place(self.s, token, self.data)  
+            self.sender.create_place(self.data)
+            
     
-        # self.data['title'] = '你好'
-        # with self.assertRaises(ConnectionError):
-        #     dict_res = create_place(self.s, token, self.data)  
+        self.data['title'] = '你好'       #может быть иероглифами
+        with self.assertRaises(ConnectionError):
+            self.sender.create_place(self.data)
     
         
         #присвоим слишком большое значение
-        self.data['title'] = 'T'*1001           #может быть 1000! тз:999 - при 1000 ломает тест
+        self.data['title'] = 'T'*1000           #может быть 1000, ТЗ:999
         with self.assertRaises(ConnectionError):
-            dict_res = create_place(self.s, token, self.data)
+            self.sender.create_place(self.data)
     
         #присвоим крайние воможные значения.
         #самые большие возможные строки разного состава
@@ -99,71 +98,68 @@ class TestCreatePlace(unittest.TestCase):
 
         for i in (digits, kir, lat, punct, all_kinds):
             self.data['title'] = i
-            dict_res = create_place(self.s, token, self.data)
+            dict_res = self.sender.create_place(self.data)
             self.assertEqual(dict_res['title'], self.data['title'])
 
         #самые маленькие значения, строки со знаками препинания, пробелами
-        with_spaces = 'я календарь переверну'
+        with_spaces = 'текст с пробелами'
         many_punc = '!@#$%^&*()_+=-\|\|\'\'//?.,;[]{`~'
 
         for i in ('1', 'й', '.', 'q', many_punc, with_spaces):
             self.data['title'] = i
-            dict_res = create_place(self.s, token, self.data)
+            dict_res = self.sender.create_place(self.data)
             self.assertEqual(dict_res['title'], self.data['title'])
      
 
     def test_lon(self):
-        token = get_token(self.s)
         self.data_init()
 
-        for i in (181, -181, inf, 'string', None):
+        for i in (181, -181, inf, 'string', None, True, complex(1, 2)):
             self.data['lon'] = i
             with self.assertRaises(ConnectionError):
-                create_place(self.s, token, self.data)
+                self.sender.create_place(self.data)
 
         for i in (180, -180, -179, 179, 0): #долгота должна быть в диапазоне [-180; 180]
             self.data['lon'] = i
-            dict_res = create_place(self.s, token, self.data)
+            dict_res = self.sender.create_place(self.data)
             self.assertEqual(dict_res['lon'], self.data['lon'])
+            self.assertEqual(type(dict_res['lon']), float)
             
 
     def test_lat(self):
-        token = get_token(self.s)
         self.data_init()
 
-        for i in (91, -91, inf, 'string', None):
+        for i in (91, -91, inf, 'string', None, True, complex(1, 2)):
             self.data['lat'] = i
             with self.assertRaises(ConnectionError):
-                create_place(self.s, token, self.data)
+                self.sender.create_place(self.data)
 
         for i in (90, 89, -90, -89, 0): #широта должна быть в диапазоне [-90; 90]
             self.data['lat'] = i
-            dict_res = create_place(self.s, token, self.data)
+            dict_res = self.sender.create_place(self.data)
             self.assertEqual(dict_res['lat'], self.data['lat'])
+            self.assertEqual(type(dict_res['lat']), float)
 
 
-    def test_color(self):
-        token = get_token(self.s)
-        
+    def test_color(self):   
         self.data_init()
         colors = ['blue',  'green', 'red', 'yellow', 'BLUE', 'GREEN', 'RED', 'YELLOW', None]
         for color in colors:
             self.data['color'] = color
-            dict_res = create_place(self.s, token, self.data)
+            dict_res = self.sender.create_place(self.data)
             self.assertEqual(dict_res['color'], color)
-        
-        #colors_wrong = [ 'darkgreen', 'chocolate', 'black', 'gray', '', 'brown', '#FFFFFF', '0', 'grey', 'pink', 'gold', 'orange', 'maroon', 'violet', 'magenta', 'purple', 'navy', 'skyblue', 'cyan', 'turquoise', 'lightgreen', 'white']
-        #for color in colors_wrong:
-        #   self.data['color'] = color
-        #   with self.assertRaises(ConnectionError):    
-        #       create_place(self.s, token, self.data) #может быть brown
+            
+        colors_wrong = [ 'darkgreen', 'chocolate', 'black', 'gray', '', 'brown', '#FFFFFF', '0', 'grey', 'pink', 'gold', 'orange', 'maroon', 'violet', 'magenta', 'purple', 'navy', 'skyblue', 'cyan', 'turquoise', 'lightgreen', 'white']
+        for color in colors_wrong:
+          self.data['color'] = color
+          with self.assertRaises(ConnectionError):    
+              self.sender.create_place(self.data) #может быть brown
 
     
-   # def test_date(self):
-   #     token = get_token(self.s)
-   #     self.data_init()
-   #     
-   #     dict_res = create_place(self.s, token, self.data)
-   #     self.assertIsNotNone(re.match(r'\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}±\d{2}:\d{2}', dict_res['created_at']))
+    def test_date(self):
+       self.data_init()
+       
+       dict_res = self.sender.create_place(self.data)
+       self.assertIsNotNone(re.match(r'\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}±\d{2}:\d{2}', dict_res['created_at']))
    #     #Регексом проверяем соответствие даты шаблону YYYY-MM-DDThh:mm:ss±hh:mm
    #     ломает тест: несоответствие формата
